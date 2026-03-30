28b6b5f7c1a84a6573e2ad892f5cf232533932ee
Talent Mundwa <talent.mundwa@students.uz.co.zw>
2026-03-30 23:20:38 +0200

    TE

--- prototype/frontend/src/components/Dashboard.js
+++ prototype/frontend/src/components/Dashboard.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { 
-  Activity, Heart, AlertCircle, TrendingUp, User, 
-  Droplet, LogOut, History, Clock, FileText, Stethoscope, 
+import {
+  Activity, Heart, AlertCircle, TrendingUp, User,
+  Droplet, LogOut, History, Clock, FileText, Stethoscope,
   ChevronRight, ArrowRight
 } from 'lucide-react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Cell } from 'recharts';
@@ -88,7 +88,6 @@ function Dashboard({ user, onLogout }) {
 
   return (
     <div className="container" style={{ maxWidth: '1400px' }}>
-      
       {/* Header */}
       <header className="dashboard-header">
         <div className="header-title-container">
@@ -100,7 +99,6 @@ function Dashboard({ user, onLogout }) {
             <p>Predicting 30-day hospital readmission risk for heart failure patients</p>
           </div>
         </div>
-        
         <div className="header-actions">
           <div className="user-profile">
             <p className="name">{user.full_name}</p>
@@ -114,17 +112,15 @@ function Dashboard({ user, onLogout }) {
 
       {/* Main Grid Layout */}
       <div className="dashboard-grid">
-        
         {/* Left Column: Input Form */}
         <div className="card" style={{ marginBottom: 0 }}>
           <form onSubmit={handleSubmit}>
-            
             {/* Section 1: Demographics */}
             <div className="form-section">
               <h3 className="section-title"><User size={20}/> Demographics & Admission</h3>
               <div className="form-row">
                 <div className="form-group">
-                  <label>Age (years;)</label>
+                  <label>Age (years)</label>
                   <input type="number" name="Age" value={formData.Age} onChange={handleChange} min="18" required />
                 </div>
                 <div className="form-group">
@@ -136,35 +132,164 @@ function Dashboard({ user, onLogout }) {
                 </div>
                 <div className="form-group">
                   <label>Admission Type</label>
-                  <select name="AdmissionType" value={formData.AdmissionType} onChange={handleChange}?���[ۈ�[YOH�SQT��S�&H��[Y\��[��O��[ۏ���[ۈ�[YOH�T��S���\��[���[ۏ���[ۈ�[YOH�SP�U�H��[X�]�O��[ۏ����[X����]���]��\�Ә[YOH��ܛKYܛ�\���X�[�[��\�[��O�X�[���[X��[YOH�[��\�[��H��[YO^ٛܛQ]K�[��\�[��_Hې�[��O^�[�P�[��_O���[ۈ�[YOH�YYX�\�H��YYX�\�O��[ۏ���[ۈ�[YOH�YYX�ZY��YYX�ZY��[ۏ���[ۈ�[YOH��]�]H���]�]O��[ۏ���[ۈ�[YOH��ݙ\��Y[����ݙ\��Y[���[ۏ���[ۈ�[YOH��[�^H���[�^O��[ۏ����[X����]����]����]����KKH�X�[ۈ���][�KO��]��\�Ә[YOH��ܛK\�X�[ۈ�����\�Ә[YOH��X�[ۋ]]H��X�]�]H�^�O^̌Kψ�][�Yۜ��ς�]��\�Ә[YOH��ܛK\��ȏ��]��\�Ә[YOH��ܛKYܛ�\���X�[��\��X��
-[R�O�X�[��[�]\OH��[X�\���[YOH��\��XД��[YO^ٛܛQ]K��\��XДHې�[��O^�[�P�[��_H�\]Z\�Yς��]���]��\�Ә[YOH��ܛKYܛ�\���X�[�X\��X��
-[R�O�X�[��[�]\OH��[X�\���[YOH�X\��XД��[YO^ٛܛQ]K�X\��XДHې�[��O^�[�P�[��_H�\]Z\�Yς��]���]��\�Ә[YOH��ܛKYܛ�\���X�[�X\��]H
-�JO�X�[��[�]\OH��[X�\���[YOH�X\��]H��[YO^ٛܛQ]K�X\��]_Hې�[��O^�[�P�[��_H�\]Z\�Yς��]���]��\�Ә[YOH��ܛKYܛ�\���X�[��\��]H
-�JO�X�[��[�]\OH��[X�\���[YOH��\��]H��[YO^ٛܛQ]K��\��]_Hې�[��O^�[�P�[��_H�\]Z\�Yς��]���]��\�Ә[YOH��ܛKYܛ�\���X�[��̈
-	JO�X�[��[�]\OH��[X�\���[YOH��̈��[YO^ٛܛQ]K��̟Hې�[��O^�[�P�[��_HZ[�H��X^H�L��\]Z\�Yς��]����]����]����KKH�X�[ۈΈX��KO��]��\�Ә[YOH��ܛK\�X�[ۈ�����\�Ә[YOH��X�[ۋ]]H����]�^�O^̌KψX�ܘ]ܞH�\�[��ς�]��\�Ә[YOH��ܛK\��ȏ��]��\�Ә[YOH��ܛKYܛ�\���X�[���
-��S
-HH�X[�[O^�ٛ۝�ZY�	ۛܛX[	���܎��͍
-���_O��[�X�]܏��X[��X�[��[�]\OH��[X�\���[YOH�����[YO^ٛܛQ]K���Hې�[��O^�[�P�[��_H�\]Z\�Yς��]���]��\�Ә[YOH��ܛKYܛ�\���X�[�ܙX][�[�H
-Y��
-O�X�[��[�]\OH��[X�\���\H��H��[YOH�ܙX][�[�H��[YO^ٛܛQ]K�ܙX][�[�_Hې�[��O^�[�P�[��_H�\]Z\�Yς��]���]��\�Ә[YOH��ܛKYܛ�\���X�[���][H
-Q\K�
-O�X�[��[�]\OH��[X�\���[YOH���][H��[YO^ٛܛQ]K���][_Hې�[��O^�[�P�[��_H�\]Z\�Yς��]���]��\�Ә[YOH��ܛKYܛ�\���X�[�[[��ؚ[�
-��
-O�X�[��[�]\OH��[X�\���\H��H��[YOH�[[��ؚ[���[YO^ٛܛQ]K�[[��ؚ[�Hې�[��O^�[�P�[��_H�\]Z\�Yς��]����]����]����KKH�X�[ۈ
-���[ܘ�Y]Y\�KO��]��\�Ә[YOH��ܛK\�X�[ۈ�����\�Ә[YOH��X�[ۋ]]H���]����H�^�O^̌Kψ�[�X�[\�ܞO�ς�]��\�Ә[YOH��ܛK\��ȏ��]��\�Ә[YOH��ܛKYܛ�\���X�[�XX�]\��X�[���[X��[YOH�XX�]\Ȉ�[YO^ٛܛQ]K�XX�]\�Hې�[��O^�[�P�[��_O���[ۈ�[YO^�O�����[ۏ��[ۈ�[YO^�_O�Y\���[ۏ����[X����]���]��\�Ә[YOH��ܛKYܛ�\���X�[�\\�[��[ۏ�X�[���[X��[YOH�\\�[��[ۈ��[YO^ٛܛQ]K�\\�[��[۟Hې�[��O^�[�P�[��_O���F���f�VS׳�������F������F���f�VS׳��W3���F������6V�V7C���F�c��F�b6�74��S�&f�&��w&�W#���&V��G&��f�'&���F������&V���6V�V7B��S�$G&��f�""f�VS׶f�&�FF�G&��f�'���6��vS׶��F�T6��vW����F���f�VS׳�������F������F���f�VS׳��W3���F������6V�V7C���F�c��F�b6�74��S�&f�&��w&�W#���&V��4�C���&V���6V�V7B��S�$4�B"f�VS׶f�&�FF�4�G���6��vS׶��F�T6��vW��(����������������������ѥ���م�Ք�����9���ѥ�����ѥ���م�Ք�����e����ѥ���(�������������������͕�����(����������������𽑥��(��������������𽑥��(������������𽑥��((��������������ɽȀ����(���������������؁��屔��쁉����ɽչ�
-����耜����ɔȜ�������耜���ňň����������耜�ɕ������ɑ��I�����耜��������ɝ��Q��耜�ɕ������(���������������������
-�ɍ���ͥ��������屔��푥�����蝥����������ɝ��I�����������ٕ�ѥ�������蝵����������(������������������ɽ��(��������������𽑥��(��������������((�������������؁�����9�����Չ��е���х���Ȉ�(�����������������ѽ��������Չ��Ј������9�����Չ��е�Ѹ����ͅ���������������(����������������������������م�Յѥ���5���������������
-���ձ�є�I�ͬ�M��ɔ����ɽ�I���Ёͥ�����������(�����������������ѽ��(������������𽑥��(����������𽙽ɴ�(��������𽑥��((�������������I���Ё
-��յ��=����Ѐ��!��ѽ�䀴��(���������؁��屔��쁑������耝������������ɕ�ѥ��耝���յ�������耜�ɕ������(����������(���������������Aɕ���ѥ���I��ձЁ	��������(�����������ɕ�ձЀ����(�������������؁�����9�������ɐ�ɕ�ձ�̵��ɐ���ɕ�ձй�ɕ���ѥ������Ā��������ɥͬ��耝��ܵɥͬ������屔��쁵�ɝ��	��ѽ�������(���������������ȁ��屔��쁑������耝������������%ѕ��耝���ѕȜ�����耜��������ɝ��耜�����ɕ��������(�����������������Qɕ�����U��ͥ�����􁍽�����ɕ�ձй�ɕ���ѥ������Ā���������М�耜��Ɍ�Ք���(����������������I�ͬ��͕�͵���(�����������������(��������������(���������������؁�����9�����ɥͬ�������ѽȀ��ɕ�ձй�ɕ���ѥ������Ā���ɥͬ�������耝ɥͬ���ܝ�����屔��홽��M��耜ĸ�ɕ�����(�����������������ɕ�ձй���ͅ���(��������������𽑥��((���������������؁��屔�q쁵�ɝ��耜�ɕ������ѕ������耝���ѕȜ����(�����������������؁��屔��쁉����ɽչ�
-����耜��ɔ�������ɑ��I�����耜�������������耜���������ͥѥ��耝ɕ��ѥٔ����ٕə���耝�����������(�������������������؀(����������������������屔���(����������������������ݥ�Ѡ聀��ɕ�ձй�ɽ�������䀨���������(���������������������������ɽչ�
-�����ɕ�ձй�ɽ������������Ԁ���������М�耜��Ɍ�Ք���(����������������������������耜������(�����������������������Ʌ�ͥѥ��耝ݥ�Ѡ��́��͔������М(����������������������(��������������������(��������������������������屔�����ͥѥ��耝��ͽ��є�������耜������ѽ�耜�������Ʌ�͙�ɴ耝�Ʌ�ͱ�є�������������������]�����耝������������耜�Ŕ��͈�������M��耜���ɕ������(��������������������I�ͬ�Aɽ����������ɕ�ձй�ɽ�������䀨������ѽ�ᕐ�ĥ��(������������������������(����������������𽑥��(��������������𽑥��((���������������؁��屔��쁵�ɝ��Q��耜�ɕ������(�����������������Ё��屔��쁍����耜������䜰���ɑ��	��ѽ�耜����ͽ������ɔ������������	��ѽ�耜���ɕ����������������ɔ�%����х������(�����������������؁��屔���ݥ�Ѡ耜�������������耜����������ɝ��Q��耜�ɕ������(�������������������I�����ٕͥ
-��х�����(���������������������	��
-���Ё��ф�확���ɕ%����х����х􁱅����ٕ�ѥ�������ɝ�����ѽ�����ɥ�����������耴�������ѽ�������(�����������������������
-��ѕͥ��ɥ����ɽ���͡��Ʌ��̀̈���ɥ齹х��홅�͕��(�����������������������a�́�����յ��Ȉ��������(�����������������������e�́��х-��􉹅��������􉍅ѕ���䈁��屔��홽��M��耜���rem'}} />
+                  <select name="AdmissionType" value={formData.AdmissionType} onChange={handleChange}>
+                    <option value="EMERGENCY">Emergency</option>
+                    <option value="URGENT">Urgent</option>
+                    <option value="ELECTIVE">Elective</option>
+                  </select>
+                </div>
+                <div className="form-group">
+                  <label>Insurance</label>
+                  <select name="Insurance" value={formData.Insurance} onChange={handleChange}>
+                    <option value="Medicare">Medicare</option>
+                    <option value="Medicaid">Medicaid</option>
+                    <option value="Private">Private</option>
+                    <option value="Government">Government</option>
+                    <option value="Self Pay">Self Pay</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            {/* Section 2: Vitals */}
+            <div className="form-section">
+              <h3 className="section-title"><Activity size={20}/> Vital Signs</h3>
+              <div className="form-row">
+                <div className="form-group">
+                  <label>Systolic BP (mmHg)</label>
+                  <input type="number" name="SystolicBP" value={formData.SystolicBP} onChange={handleChange} required />
+                </div>
+                <div className="form-group">
+                  <label>Diastolic BP (mmHg)</label>
+                  <input type="number" name="DiastolicBP" value={formData.DiastolicBP} onChange={handleChange} required />
+                </div>
+                <div className="form-group">
+                  <label>Heart Rate (bpm)</label>
+                  <input type="number" name="HeartRate" value={formData.HeartRate} onChange={handleChange} required />
+                </div>
+                <div className="form-group">
+                  <label>Resp Rate (bpm)</label>
+                  <input type="number" name="RespRate" value={formData.RespRate} onChange={handleChange} required />
+                </div>
+                <div className="form-group">
+                  <label>SpO2 (%)</label>
+                  <input type="number" name="SpO2" value={formData.SpO2} onChange={handleChange} min="0" max="100" required />
+                </div>
+              </div>
+            </div>
+
+            {/* Section 3: Labs */}
+            <div className="form-section">
+              <h3 className="section-title"><Droplet size={20}/> Laboratory Results</h3>
+              <div className="form-row">
+                <div className="form-group">
+                  <label>BNP (pg/mL)</label>
+                  <input type="number" name="BNP" value={formData.BNP} onChange={handleChange} required />
+                </div>
+                <div className="form-group">
+                  <label>Creatinine (mg/dL)</label>
+                  <input type="number" step="0.1" name="Creatinine" value={formData.Creatinine} onChange={handleChange} required />
+                </div>
+                <div className="form-group">
+                  <label>Sodium (mEq/L)</label>
+                  <input type="number" name="Sodium" value={formData.Sodium} onChange={handleChange} required />
+                </div>
+                <div className="form-group">
+                  <label>Hemoglobin (g/dL)</label>
+                  <input type="number" step="0.1" name="Hemoglobin" value={formData.Hemoglobin} onChange={handleChange} required />
+                </div>
+              </div>
+            </div>
+
+            {/* Section 4: Comorbidities */}
+            <div className="form-section">
+              <h3 className="section-title"><Stethoscope size={20}/> Clinical History</h3>
+              <div className="form-row">
+                <div className="form-group">
+                  <label>Diabetes</label>
+                  <select name="Diabetes" value={formData.Diabetes} onChange={handleChange}>
+                    <option value={0}>No</option><option value={1}>Yes</option>
+                  </select>
+                </div>
+                <div className="form-group">
+                  <label>Hypertension</label>
+                  <select name="Hypertension" value={formData.Hypertension} onChange={handleChange}>
+                    <option value={0}>No</option><option value={1}>Yes</option>
+                  </select>
+                </div>
+                <div className="form-group">
+                  <label>Atrial Fibrillation</label>
+                  <select name="AtrialFib" value={formData.AtrialFib} onChange={handleChange}>
+                    <option value={0}>No</option><option value={1}>Yes</option>
+                  </select>
+                </div>
+                <div className="form-group">
+                  <label>COPD</label>
+                  <select name="COPD" value={formData.COPD} onChange={handleChange}>
+                    <option value={0}>No</option><option value={1}>Yes</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            {error && (
+              <div style={{ backgroundColor: '#fee2e2', color: '#991b1b', padding: '1rem', borderRadius: '8px', marginTop: '1rem' }}>
+                <AlertCircle size={16} style={{display:'inline', marginRight:'8px', verticalAlign:'middle'}}/>
+                {error}
+              </div>
+            )}
+
+            <div className="submit-container">
+              <button type="submit" className="submit-btn" disabled={loading}>
+                {loading ? 'Evaluating Model...' : <><i> Calculate Risk Score  <ArrowRight size={18}/></i></>}
+              </button>
+            </div>
+          </form>
+        </div>
+
+        {/* Right Column: Output & History */}
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '2rem' }}>
+          
+          {/* Prediction Result Block */}
+          {result && (
+            <div className={`card results-card ${result.prediction === 1 ? 'high-risk' : 'low-risk'}`} style={{ marginBottom: 0 }}>
+              <h2 style={{ display: 'flex', alignItems: 'center', gap: '8px', margin: '0 0 1rem 0' }}>
+                <TrendingUp size={24} color={result.prediction === 1 ? '#ef4444' : '#22c55e'} />
+                Risk Assessment
+              </h2>
+              
+              <div className={`risk-indicator ${result.prediction === 1 ? 'risk-high' : 'risk-low'}`} style={{fontSize: '1.4rem'}}>
+                {result.message}
+              </div>
+
+              <div style={{ margin: '2rem 0', textAlign: 'center' }}>
+                <div style={{ backgroundColor: '#e2e8f0', borderRadius: '20px', height: '28px', position: 'relative', overflow: 'hidden' }}>
+                  <div 
+                    style={{ 
+                      width: `${result.probability * 100}%`, 
+                      backgroundColor: result.probability > 0.5 ? '#ef4444' : '#22c55e', 
+                      height: '100%',
+                      transition: 'width 1s ease-in-out'
+                    }} 
+                  />
+                  <span style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', fontWeight: 'bold', color: '#1e293b', fontSize: '0.9rem' }}>
+                    Risk Probability: {(result.probability * 100).toFixed(1)}%
+                  </span>
+                </div>
+              </div>
+
+              <div style={{ marginTop: '2rem' }}>
+                <h4 style={{ color: '#475569', borderBottom: '1px solid #e2e8f0', paddingBottom: '0.5rem' }}>Global Feature Importance</h4>
+                <div style={{ width: '100%', height: '160px', marginTop: '1rem' }}>
+                  <ResponsiveContainer>
+                    <BarChart data={featureImportanceData} layout="vertical" margin={{ top: 0, right: 0, left: -20, bottom: 0 }}>
+                      <CartesianGrid strokeDasharray="3 3" horizontal={false} />
+                      <XAxis type="number" hide />
+                      <YAxis dataKey="name" type="category" style={{fontSize: '0.8rem'}} />
                       <Tooltip />
                       <Bar dataKey="value" fill="#3b82f6" radius={[0, 4, 4, 0]}>
                         {featureImportanceData.map((entry, index) => (
-                          <Cell key={`cell-${index}`} fill={yndex < 2 ? '#2563eb' : '#93c5fd'} />
+                          <Cell key={`cell-${index}`} fill={index < 2 ? '#2563eb' : '#93c5fd'} />
                         ))}
                       </Bar>
                     </BarChart>
@@ -191,8 +316,8 @@ O�X�[��[�]\OH��[X�\���\H�
                     </div>
                     <div className="history-body">
                       <span className="history-patient">Age: {item.patient_age}</span>
-                      <span className=`badge ${item.risk_level === 'High' ? 'badge-high' : 'badge-low'}` style={{fontSize: '0.75rem'}>
-                        {(fitem.probability * 100).toFixed(1)}% Risk
+                      <span className={`badge ${item.risk_level === 'High' ? 'badge-high' : 'badge-low'}`} style={{fontSize: '0.75rem'}}>
+                        {(item.probability * 100).toFixed(1)}% Risk
                       </span>
                     </div>
                   </div>
